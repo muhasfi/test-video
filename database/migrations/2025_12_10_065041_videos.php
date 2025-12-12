fc3d6192f59e082e64a96b2d8f8f57c8b898d95c
Muhammad Hashfi <106295328+muhasfi@users.noreply.github.com>
2025-12-12 15:05:09 +0700

    Add softDeletes to videos migration
    Added soft deletes to the videos table migration.

--- database/migrations/2025_12_10_065041_videos.php
+++ database/migrations/2025_12_10_065041_videos.php
@@ -12,12 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('videos', function (Blueprint $table) {
-        $table->id();
-        $table->string('title');
-        $table->text('description')->nullable();
-        $table->string('video_url'); // isi link atau path file
-        $table->timestamps();
-    });
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('video_url'); // isi link atau path file
+            $table->timestamps();
+            $table->softDeletes();
+        });
     }
 
     /**
